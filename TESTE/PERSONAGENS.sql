8c6bb73bc408bbdf0128ef789fd6d5b9fd52f47a
matheuxmuller <67790499+matheuxmuller@users.noreply.github.com>
2020-07-23 16:52:38 -0300

    Delete PERSONAGENS.sql

--- TESTE/PERSONAGENS.sql
+++
@@ -1,4 +0,0 @@
-select tb_personagem.nome, tb_classe.tipo_poder, tb_classe.ataque, tb_classe.defesa
-from tb_personagem
-       inner join tb_classe on tb_classe.id = tb_personagem.classe_id
-       where tipo_poder like "Pirotecnia"
